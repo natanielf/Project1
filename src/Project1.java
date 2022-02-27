c2b26fda575b4cd198ad1922d77973711a56e7d4
natanielf <natanielfarzan@gmail.com>
2022-02-27 12:18:51 -0800

    Rename class and implement `textToCoordinate()` method

--- src/Project1.java
+++ src/Project1.java
@@ -2,14 +2,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class p1 {
+public class Project1 {
 
 	private int rows, cols, rooms;
 	private char[][] map;
 	private Scanner s;
 	private boolean isTextBased;
 
-	public p1(File f) {
+	public Project1(File f) {
 		try {
 			this.s = new Scanner(f);
 			this.rows = s.nextInt();
@@ -32,13 +32,15 @@ public class p1 {
 	public static void main(String[] args) {
 		File f1 = new File("./maps/map1t.txt");
 		System.out.println(f1.getPath());
-		p1 p1 = new p1(f1);
+		Project1 p1 = new Project1(f1);
 		System.out.println(p1);
 
 		File f2 = new File("./maps/map1c.txt");
 		System.out.println(f2.getPath());
-		p1 p2 = new p1(f2);
+		Project1 p2 = new Project1(f2);
 		System.out.println(p2);
+
+		System.out.println(Project1.textToCoordinate(f1));
 	}
 
 	public void textBased() {
@@ -63,6 +65,34 @@ public class p1 {
 			int row = Character.getNumericValue(line[2]);
 			int col = Character.getNumericValue(line[4]);
 			map[row][col] = line[0];
+		}
+	}
+
+	public static String textToCoordinate(File f) {
+		boolean isTextBased = f.getPath().substring(0, f.getPath().length() - 4).contains("t");
+		if (!isTextBased)
+			return null;
+
+		try {
+			Scanner s = new Scanner(f);
+			String str = "";
+			str += s.nextLine() + "\n";
+			int row = 0;
+			while (s.hasNextLine()) {
+				char[] line = s.nextLine().toCharArray();
+				for (int i = 0; i < line.length; i++) {
+					char curr = line[i];
+					if (curr != '.') {
+						str += Character.toString(curr) + " " + row + " " + i;
+						str += "\n";
+					}
+				}
+				row++;
+			}
+			return str;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return null;
 		}
 	}
 
