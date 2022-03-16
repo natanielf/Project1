454ebeb13516de65e0cce4d686302814a7ce9eb9
natanielf <natanielfarzan@gmail.com>
2022-03-15 18:48:45 -0700

    Clean up move methods, add coordinate-based solution printing

--- src/p1.java
+++ src/p1.java
@@ -50,6 +50,9 @@ public class p1 {
 		this.rows = s.nextInt();
 		this.cols = s.nextInt();
 		this.rooms = s.nextInt();
+
+		checkMapParameters();
+
 		this.map = new Tile[rows][cols];
 		this.s.nextLine();
 		this.foundCake = false;
@@ -61,12 +64,19 @@ public class p1 {
 		if (queueApproach) {
 			initQueue();
 			queueMove();
-			if (printTextBased) {
-				printQueuePathTextBased();
+			if (!foundCake) {
+				System.out.println("The cake is a lie.");
+				System.exit(1);
 			}
+			printQueuePath();
 		} else if (stackApproach) {
 			initStack();
 			stackMove();
+			printStackPath();
+			if (!foundCake) {
+				System.out.println("The cake is a lie.");
+				System.exit(1);
+			}
 		} else {
 			// optimal approach
 		}
@@ -77,49 +87,52 @@ public class p1 {
 		p1 p = new p1(args);
 	}
 
-	public void printQueuePathTextBased() {
-		Tile[][] solution = new Tile[rows][cols];
-		for (int r = 0; r < map.length; r++) {
-			for (int c = 0; c < map[r].length; c++) {
-				solution[r][c] = new Tile(map[r][c].getValue());
-			}
-		}
-		int[] curr = null;
-		int[] next = dequeue.remove();
-		while (dequeue.size() > 0) {
-			curr = next;
-			next = dequeue.remove();
-			if ((curr[0] != kirby[0] || curr[1] != kirby[1]) && (curr[0] != cake[0] || curr[1] != cake[1])) {
-				if (!solution[curr[0]][curr[1]].isVisited()) {
-					if ((curr[0] == next[0] && curr[1] == next[1] || curr[1] == next[1] + 1 || curr[1] == next[1] - 1)
-							|| (curr[1] == next[1] && curr[0] == next[0] || curr[0] == next[0] + 1
-									|| curr[0] == next[0] - 1)) {
-						solution[curr[0]][curr[1]].setValue('+');
-						solution[curr[0]][curr[1]].setVisited(true);
-					}
+	public void printQueuePath() {
+		Tile[][] solution = map.clone();
+
+		if (printTextBased) {
+			while (dequeue.size() > 0) {
+				int[] curr = dequeue.remove();
+				int row = curr[0];
+				int col = curr[1];
+
+				if ((row != kirby[0] || col != kirby[1]) && (row != cake[0] || col != cake[1])) {
+					solution[curr[0]][curr[1]].setValue('+');
 				}
-
 			}
-
+			System.out.println(toString(solution));
+		} else {
+			while (dequeue.size() > 0) {
+				int[] curr = dequeue.remove();
+				int row = curr[0];
+				int col = curr[1];
+				System.out.println("+ " + row + " " + col);
+			}
 		}
-		System.out.println(toString(solution));
 	}
 
-	public void printQueuePathTextBased1() {
-		Tile[][] solution = new Tile[rows][cols];
-		for (int r = 0; r < map.length; r++) {
-			for (int c = 0; c < map[r].length; c++) {
-				solution[r][c] = new Tile(map[r][c].getValue());
-			}
-		}
+	public void printStackPath() {
+		Tile[][] solution = map.clone();
 
-		while (dequeue.size() > 0) {
-			int[] curr = dequeue.remove();
-			if ((curr[0] != kirby[0] || curr[1] != kirby[1]) && (curr[0] != cake[0] || curr[1] != cake[1])) {
-				solution[curr[0]][curr[1]].setValue('+');
+		if (printTextBased) {
+			while (outstack.size() > 0) {
+				int[] curr = outstack.pop();
+				int row = curr[0];
+				int col = curr[1];
+
+				if ((row != kirby[0] || col != kirby[1]) && (row != cake[0] || col != cake[1])) {
+					solution[row][col].setValue('+');
+				}
+			}
+			System.out.println(toString(solution));
+		} else {
+			while (outstack.size() > 0) {
+				int[] curr = outstack.pop();
+				int row = curr[0];
+				int col = curr[1];
+				System.out.println("+ " + row + " " + col);
 			}
 		}
-		System.out.println(toString(solution));
 	}
 
 	public void findKirby() {
@@ -133,6 +146,7 @@ public class p1 {
 				}
 			}
 		}
+
 		this.kirby = new int[] { kR, kC };
 		map[kR][kC].setVisited(true);
 	}
@@ -155,91 +169,73 @@ public class p1 {
 
 	public void queueMove() {
 		int[] coordinates = enqueue.remove();
-		dequeue.add(coordinates);
 		int row = coordinates[0];
 		int col = coordinates[1];
+
+		dequeue.add(coordinates);
+		map[row][col].setVisited(true);
 
 		if (tileHoldsCake(row, col)) {
 			foundCake = true;
 			cake = new int[] { row, col };
 		}
 
-		map[row][col].setVisited(true);
-
 		if (!foundCake) {
 
 			// North
-			if (moveIsValid(row - 1, col)) {
-				int[] newCoordinates = { row - 1, col };
-				map[row - 1][col].setVisited(true);
-				enqueue.add(newCoordinates);
-			}
+			if (moveIsValid(row - 1, col))
+				enqueue.add(new int[] { row - 1, col });
 
 			// South
-			if (moveIsValid(row + 1, col)) {
-				int[] newCoordinates = { row + 1, col };
-				map[row + 1][col].setVisited(true);
-				enqueue.add(newCoordinates);
-			}
+			if (moveIsValid(row + 1, col))
+				enqueue.add(new int[] { row + 1, col });
 
 			// East
-			if (moveIsValid(row, col + 1)) {
-				map[row][col + 1].setVisited(true);
-				int[] newCoordinates = { row, col + 1 };
-				enqueue.add(newCoordinates);
-			}
+			if (moveIsValid(row, col + 1))
+				enqueue.add(new int[] { row, col + 1 });
 
 			// West
-			if (moveIsValid(row, col - 1)) {
-				int[] newCoordinates = { row, col - 1 };
-				map[row][col - 1].setVisited(true);
-				enqueue.add(newCoordinates);
-			}
+			if (moveIsValid(row, col - 1))
+				enqueue.add(new int[] { row, col - 1 });
 
-			queueMove();
+			if (enqueue.size() > 0)
+				queueMove();
 		}
 	}
 
 	public void stackMove() {
 		int[] coordinates = instack.pop();
-		outstack.push(coordinates);
 		int row = coordinates[0];
 		int col = coordinates[1];
 
+		outstack.push(coordinates);
+		map[row][col].setVisited(true);
+
 		if (tileHoldsCake(row, col)) {
 			foundCake = true;
-			this.cake = new int[] { row, col };
+			cake = new int[] { row, col };
 		}
-
-		map[row][col].setVisited(true);
 
 		if (!foundCake) {
 
 			// North
-			if (moveIsValid(row - 1, col)) {
-				int[] newCoordinates = { row - 1, col };
-				instack.push(newCoordinates);
-			}
+			if (moveIsValid(row - 1, col))
+				instack.push(new int[] { row - 1, col });
 
 			// South
-			if (moveIsValid(row + 1, col)) {
-				int[] newCoordinates = { row + 1, col };
-				instack.push(newCoordinates);
-			}
+			if (moveIsValid(row + 1, col))
+				instack.push(new int[] { row + 1, col });
 
 			// East
-			if (moveIsValid(row, col + 1)) {
-				int[] newCoordinates = { row, col + 1 };
-				instack.push(newCoordinates);
-			}
+			if (moveIsValid(row, col + 1))
+				instack.push(new int[] { row, col + 1 });
 
 			// West
-			if (moveIsValid(row, col - 1)) {
-				int[] newCoordinates = { row, col - 1 };
-				instack.push(newCoordinates);
-			}
+			if (moveIsValid(row, col - 1))
+				instack.push(new int[] { row, col - 1 });
 
-			stackMove();
+			if (instack.size() > 0)
+				stackMove();
 		}
 	}
 
@@ -386,6 +382,9 @@ public class p1 {
 	public void checkCLIArguments(String[] args) {
 		int numApproachArgs = 0;
 		boolean displayHelp = false;
+		this.queueApproach = false;
+		this.stackApproach = false;
+		this.optimalApproach = false;
 		for (String arg : args) {
 			if (arg.equals("--Queue")) {
 				this.queueApproach = true;
@@ -433,6 +432,13 @@ public class p1 {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
+		}
+	}
+
+	public void checkMapParameters() {
+		if (rows <= 0 || cols <= 0 || rooms <= 0) {
+			System.err.println("Error: Invalid map parameters. Maps must have more than 0 rows, columns, and rooms.");
+			System.exit(-1);
 		}
 	}
 }
